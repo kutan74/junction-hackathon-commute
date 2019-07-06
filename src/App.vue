f0c7b9242e2638a9c99d3c9a036284a641c9f8d9
System Administrator <root@KUTANs-MacBook-Pro.local>
2019-07-06 17:36:38 +0300

    created landing page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="appLogo" alt="Vue logo" src="./assets/logo_white_background.jpg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import './style.css'
 
 export default {
   name: 'app',
